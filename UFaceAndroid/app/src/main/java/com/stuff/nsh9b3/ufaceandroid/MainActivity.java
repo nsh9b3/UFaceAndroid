903157640166f8ae9ca081946d78b2b8fbc608ad
Nick Hilbert <nsh9b3@mst.edu>
2016-11-22 17:00:39 -0600

    Main Activity has options for making new buttons and is mostly setup. Added a new activity for selecting a new service to register with.

--- UFaceAndroid/app/src/main/java/com/stuff/nsh9b3/ufaceandroid/MainActivity.java
+++ UFaceAndroid/app/src/main/java/com/stuff/nsh9b3/ufaceandroid/MainActivity.java
@@ -1,15 +1,119 @@
 package com.stuff.nsh9b3.ufaceandroid;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    // List of Buttons (services) a user can select
+    public static ArrayList<Button> buttonList;
+
+    // List of layouts (rows of services) to place new services
+    private ArrayList<LinearLayout> layoutList;
+
+    // These are offset values so btns and layouts have different IDs
+    private final static int btnIDOffset = 1000;
+    private final static int layIDOffset = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the add button and set the listener to this activity (which is a clickListener)
+        final Button addButton = (Button)findViewById(R.id.btn_add);
+        addButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        // First figure out what was pressed
+        switch(view.getId())
+        {
+            // If the add button was pressed, register a new web service
+            case R.id.btn_add:
+                Intent newServiceIntent = new Intent(this, SelectNewService.class);
+                break;
+            // Otherwise, authenticate a user on a specific web service
+            default:
+                break;
+        }
+    }
+
+    private void getServices()
+    {
+        buttonList = new ArrayList<>();
+        layoutList = new ArrayList<>();
+
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Set<String> servList = sharedPref.getStringSet(SharedPrefKeys.SERVICE_LIST, new HashSet<String>());
+
+        /*for (Iterator<String> it = servList.iterator(); it.hasNext(); ) {
+            String service = it.next();
+            makeNewServiceIcon(service);
+        }*/
+    }
+
+    private void makeNewServiceIcon(String serviceName)
+    {
+        LinearLayout parentLayout = (LinearLayout)findViewById(R.id.ll_parent);
+
+        int row = (int) (buttonList.size() / 3);
+        int col = buttonList.size() % 3;
+
+        LinearLayout childLayout;
+
+        // Make a new row to place the button
+        if(col == 0)
+        {
+            childLayout = new LinearLayout(this);
+            childLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            childLayout.setLayoutParams(layoutParams);
+            childLayout.setWeightSum(3);
+
+            childLayout.setId(layoutList.size() + layIDOffset);
+
+            parentLayout.addView(childLayout);
+            layoutList.add(childLayout);
+        }
+        // Grab an existing location for the button
+        else
+        {
+            childLayout = (LinearLayout)findViewById(layoutList.get((layoutList.size() - 1)).getId());
+        }
+
+        Button newServiceBtn = new Button(this);
+        LinearLayout.LayoutParams btnParams = new LinearLayout.LayoutParams(new GridView.LayoutParams(0, (int)(getResources().getDisplayMetrics().density * 100 + 0.5f)));
+        btnParams.setMargins((int)getResources().getDimension(R.dimen.activity_horizontal_margin),
+                (int)getResources().getDimension(R.dimen.activity_vertical_margin),
+                (int)getResources().getDimension(R.dimen.activity_horizontal_margin),
+                (int)getResources().getDimension(R.dimen.activity_vertical_margin));
+        btnParams.weight = 1;
+
+        newServiceBtn.setLayoutParams(btnParams);
+        newServiceBtn.setText(serviceName);// + " - " + userID + " - " + userIndex);
+        newServiceBtn.setId(buttonList.size() + btnIDOffset);
+        newServiceBtn.setOnClickListener(this);
+
+        childLayout.addView(newServiceBtn);
+        buttonList.add(newServiceBtn);
     }
 }
---
+++ UFaceAndroid/app/src/main/java/com/stuff/nsh9b3/ufaceandroid/SelectNewService.java
@@ -0,0 +1,15 @@
+package com.stuff.nsh9b3.ufaceandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectNewService extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_new_service);
+    }
+}
