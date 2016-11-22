f03c65a13f77fe4cc26bcde4c7e6680cecfd92e3
Nick Hilbert <nsh9b3@mst.edu>
2016-11-21 23:36:18 -0600

    Added files needed for project

---
+++ UFaceAndroid/app/src/main/java/com/stuff/nsh9b3/ufaceandroid/LBP.java
@@ -0,0 +1,9 @@
+package com.stuff.nsh9b3.ufaceandroid;
+
+/**
+ * Created by nick on 11/21/16.
+ */
+
+public class LBP
+{
+}
--- UFaceAndroid/app/src/main/java/com/stuff/nsh9b3/ufaceandroid/RegisterWebService.java
+++ UFaceAndroid/app/src/main/java/com/stuff/nsh9b3/ufaceandroid/RegisterWebService.java
@@ -3,13 +3,13 @@ package com.stuff.nsh9b3.ufaceandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
+public class RegisterWebService extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register_web_service);
     }
 }
